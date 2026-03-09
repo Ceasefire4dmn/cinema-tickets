92d4a0ccf01e2b9522b06f7e30fa6400d6e9b736
Trunova <alext.2005@mail.ru>
2026-03-09 02:44:13 -0400

    fix2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { DatabaseProvider } from './contexts/DatabaseProvider';
 import { Layout } from './components/Layout';
 import Afisha from './pages/Afisha/Afisha.jsx';
@@ -6,7 +6,7 @@ import MovieDetails from './pages/MovieDetails/MovieDetails';
 import HallDetails from './pages/HallDetails/HallDetails.jsx'
 import HallView from './pages/HallView/HallView.jsx';
 import HallSelection from './pages/HallSelection/HallSelection.jsx';
-import Checkout from './pages/Checkout/Checkout.jsx'; 
+import Checkout from './pages/Checkout/Checkout.jsx';
 
 function EmptyPage() {
   return <div className="glass p-8 rounded-xl">Страница в разработке. БД готова для демо (изменения в памяти).</div>;
@@ -14,7 +14,7 @@ function EmptyPage() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <DatabaseProvider>
         <Routes>
           <Route path="/" element={<Layout />}>
@@ -24,11 +24,11 @@ function App() {
             <Route path='/hall' element={<HallDetails />} />
             <Route path="/hall-info/:hallId" element={<HallView />} />
             <Route path="/hall-selection/:showtimeId" element={<HallSelection />} />
-            <Route path="/checkout/:showtimeId" element={<Checkout />} /> 
+            <Route path="/checkout/:showtimeId" element={<Checkout />} />
           </Route>
         </Routes>
       </DatabaseProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
